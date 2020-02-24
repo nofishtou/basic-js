3ea44c11ee12629caca75cbb192054f5f91bc43e
nofishtou <damnedbyhimself@gmail.com>
2020-02-24 20:35:25 +0200

    feat: transform-array and carbon-dating are done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) === 'string' && 0 < parseFloat(sampleActivity) && parseFloat(sampleActivity) < 15){
+    const k = 0.693/ HALF_LIFE_PERIOD 
+    let result = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/k) 
+    return result
+  } else {
+    return false
+  }
+
+  // throw 'Not implemented';
   // remove line with error and write your code here
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,71 +1,35 @@
 module.exports = function transform(arr) {
-    // if(typeof(arr) === 'object' && arr !== []){
-    //     let mark = true
-    //     let counter = 0
-    //     const result = arr.reduce((res, item, index, array) => {
-    //         if(mark){
-    //             if(item === '--discard-next') {
-    //                 mark = false;
-    //                 counter += 2
-    //                 return res
-    //             }
-    //             if(item === '--discard-prev'){      
-    //                 res.splice(index - counter, 1)
-    //                 counter +=2
-    //                 return res
-    //             }
-    //             if(item === '--double-next'){
-    //                 if(index !== array.length - 1){
-    //                     res.push(array[index + 1]);
-    //                     return res
-    //                 }   
-    //                 return res
-    //             }
-    //             if(item === '--double-prev'){
-    //                 if(index !== 0){
-    //                     res.push(array[index - 1]);
-    //                     return res
-    //                 }
-    //                 return res
-    //             }
-    //             res.push(item)
-    //             return res
-    //         } else {
-    //             mark = true;
-    //             return res
-    //         }
-    //     }, []) 
-    //     return result
-    // } 
-    if(typeof(arr) === 'object' && arr !== []){
-        const result = [...arr]
-        result.forEach((item, index, array) => {
+      if(typeof(arr) === 'object' && arr !== []){
+        const result = []
+        let push = true
+  
+        arr.forEach((item, index, array) => {
+          if(push) {
             if(item === '--discard-next') {
-                array.splice(index + 1, 1)
-                array.splice(index, 1)
+              if(index !== array.length - 1){
+                push = false
+              }  
+            } else if (item === '--discard-prev') {            
+              if(index !== 0){
+                result.splice(result.length -1, 1)
+              }
+            } else if(item === '--double-next') {
+              if(index !== array.length - 1) {
+                  result.push(array[index +1])
+              }    
+            } else if(item === '--double-prev') {
+              if(index !== 0){
+                result.push(array[index - 1])
+              } 
+            } else {
+              return result.push(item)
             }
-            if(item === '--discard-prev'){
-                array.splice(index, 1)
-                array.splice(index - 1, 1)   
-            }
-            if(item === '--double-next'){
-                if(index !== array.length - 1){
-                    array.splice(index, 1, array[index + 1])
-                } else {
-                    array.splice(index, 1) 
-                }   
-            }
-            if(item === '--double-prev'){
-                if(index !== 0){
-                    array.splice(index, 1, array[index - 1])
-                } else {
-                    array.splice(index, 1) 
-                }
-            }
-            return item
+          } else {
+            push = true;
+          }
         })
         return result
-    }
+      }
     throw 'Not array';
     // remove line with error and write your code here
 };
