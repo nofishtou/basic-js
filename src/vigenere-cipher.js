294938a8cf789b9f89ddca06ad8b8d42c333d49b
nofishtou <damnedbyhimself@gmail.com>
2020-02-29 17:31:09 +0200

    feat: hanoi-tower,vigenere-cipher, what-season are done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,91 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(type = true) {
+        this.direct = type;	
     }
+    encrypt(m, k) {
+      m = m.toUpperCase()
+      k = k.toUpperCase()
+      if(m !== undefined && k !== undefined){
+        let result = '';
+        let counter = 0;
+        let r = '';	
+        let text = ''
 
-    decrypt() {
+        for(let i = 0; i < m.length;i++) {
+          if( 65 <= m[i].charCodeAt() && m[i].charCodeAt() <= 90){
+            text +=m[i]
+          }
+        }
+ 
+        for(let i = 0; i < text.length; i++  ){	
+          let mi = text[i].charCodeAt();
+          let ki_s = k[ ( (i>=k.length) ?(i%k.length) : i ) ];
+          let ki = ki_s.charCodeAt()
+          let c = String.fromCodePoint( (( 26 +  (mi + ki )) % 26) + 65  )		
+          r += c;	
+        }
+
+        for(let i=0; i < m.length; i++) {
+          if(65 <= m[i].charCodeAt() && m[i].charCodeAt() <= 90) {
+            result += r[i - counter]
+          } else {
+            counter++
+            result +=m[i]
+          }
+        }
+
+        if(this.direct) {
+          return result
+        } else {
+          return result.split('').reverse().join('')
+        }
+        
+      } else {
         throw 'Not implemented';
-        // remove line with error and write your code here
+      }
+    }
+  
+    decrypt(m, k) {
+      m = m.toUpperCase()
+      k = k.toUpperCase()
+      if(m !== undefined && k !== undefined) {      
+        let r = '';	
+        let text = ''
+        let result = '';
+        let counter = 0;
+
+        for(let i = 0; i < m.length;i++){
+          if( 65 <= m[i].charCodeAt() && m[i].charCodeAt() <= 90) {
+            text +=m[i]
+          }
+        }
+ 
+        for(let i = 0; i < text.length; i++  ){	
+          let mi = text[i].charCodeAt();
+          let ki_s = k[ ( (i >= k.length) ?(i % k.length) : i ) ];
+          let ki = ki_s.charCodeAt()
+          let c = String.fromCodePoint( ((26 + (mi - ki)) % 26) + 65 )		
+          r += c;	
+        }
+
+        for(let i=0; i < m.length; i++) {
+          if(65 <= m[i].charCodeAt() && m[i].charCodeAt() <= 90) {
+            result += r[i - counter]
+
+          } else {
+            counter++
+            result +=m[i]
+          }
+        }
+
+        if(this.direct) {
+          return result
+        } else {
+          return result.split('').reverse().join('')
+        }
+        } else {
+            throw 'Not implemented';
+        }
     }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {   
+    if(date !== undefined) {
+      if(!date.hasOwnProperty('getDay')) {
+        let month = date.getMonth() 
+        if( month === 0 || month === 1 || month === 11){
+          return 'winter';
+        } else if ( month > 1 && month < 5) {
+          return 'spring'
+        } else if ( month > 4 && month < 8) {
+          return 'summer'
+        } else {
+          return 'autumn'
+        } 
+    } else {
+      throw 'error'
+    }
+  } else {
+    return 'Unable to determine the time of year!'
+  }
 };
